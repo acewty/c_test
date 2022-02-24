68085d3995dc8b94683099e649097f4c3b985c55
acewty <1945560886@qq.com>
2022-02-24 17:49:41 +0800

    增加了C语言练习题第一个

--- main/test.c
+++ main/test.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <float.h>
-#include "qcit_types.h"
+#include "./../include/qcit_types.h"
 #define KEY 1
 
 //uint8* p_g_u8_test1;
 
+
 int main(void)
 {
-	int8 i8_i = -8;
-	uint8 u8_j = 10;
-	printf("%x",u8_j + i8_i);
+	//uint8 u8_i, u8_j, u8_k, u8_num = 0;
+	//uint16 u16_val[24];
+	for(u8_i = 1; u8_i < 5; ++u8_i)
+	{
+		for(u8_j = 1; u8_j < 5; ++u8_j)
+		{
+			for(u8_k = 1; u8_k < 5; ++u8_k)
+			{
+				if(u8_i != u8_j && u8_i != u8_k && u8_j != u8_k)
+				{
+					printf("%d\n",u8_i*100 + u8_j*10 + u8_k);
+					u8_num++;
+				}
+			}
+		}
+	}
+	printf("数量u8_num:%d\n",u8_num);
 }
 
-
+/*
 int main1(void)
 {
 	float f_inch_cm = 2.54;
@@ -31,5 +46,7 @@ int main1(void)
 	printf("sizeof(short):%d; sizeof(float):%d; sizeof(double):%d; %d; %d", sizeof(short), sizeof(float), sizeof(double), sizeof(long long), sizeof f_inch_cm);
 	
 	printf("增加测试");
+	printf("增加测试2");
 	return 0;
 }
+*/
